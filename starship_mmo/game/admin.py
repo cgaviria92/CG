73ce9e75bf4ffed854ec80bb98c0c325eedc9f69
CarlosGaviria <carlosgaviria@pasarltda.com>
2024-11-14 17:55:14 -0500

    cambios se elimina proyecto

--- starship_mmo/game/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Player, Ship, Map, NPC
-
-admin.site.register(Player)
-admin.site.register(Ship)
-admin.site.register(Map)
-admin.site.register(NPC)
